9952c569699a8e6d3faea96e002d5a1ab9cfe116
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-02 22:29:37 -0300

    ex01 realizado com sucesso

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula1/ex01/script.js
@@ -0,0 +1,7 @@
+let horasTrabalhadas = parseFloat(prompt('Informe a quantidade de horas trabalhadas: '))
+
+let valorHora = parseFloat(prompt('Informe o valor(R$) do seu trabalho por hora: '))
+
+let op = horasTrabalhadas * valorHora
+
+alert(`O seu salário é de R$${op}`)
