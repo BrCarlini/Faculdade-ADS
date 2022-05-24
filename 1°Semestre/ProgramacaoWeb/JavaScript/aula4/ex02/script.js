ab9581a5e7a1ec483d7ce4d94db1deaadb0edf8f
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-23 23:39:16 -0300

    Ex 2 da aula4 de PW concluída

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex02/script.js
@@ -0,0 +1,9 @@
+/*
+2. Modifique o programa anterior para que exiba os números ímpares de 1 a 37.
+*/
+
+for(i = 1; i <= 37; i++){
+    if(i % 2 == 1){
+        alert(`Números impares de 1 a 37: ${i}`)
+    }
+}
