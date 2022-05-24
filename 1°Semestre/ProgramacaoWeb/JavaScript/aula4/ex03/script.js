d5cc9bca0c45d738c9f81f1e177def264e7e8065
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-23 23:50:57 -0300

    Finalizar -> Ex 3 da aula4 e Ex 3 da aula3

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex03/script.js
@@ -0,0 +1,8 @@
+/*
+3. Escreva um programa que exiba qualquer tabuada completa. O programa deve perguntar ao 
+usuário qual tabuada ele deseja visualizar. O usuário deverá responder digitando um 
+número de 1 a 10 e a tabuada deverá ser exibida na página.
+*/
+
+
+let tabuada = parseInt(prompt("Informe a tabuada que deseja ver: "))
