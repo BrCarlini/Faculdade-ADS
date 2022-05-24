970d659dc8ab89896d63965a9ad37a10d59a16cc
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-23 23:24:51 -0300

    Ex 1 da aula4 de PW concluída

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex01/script.js
@@ -0,0 +1,11 @@
+/*
+1. Escreva um programa em JavaScript que exiba os números pares de 2 a 12 utilizando laços 
+de repetição e um alerta.
+
+*/
+
+for(i = 1; i <= 12; i++){
+    if(i % 2 == 0){
+        alert(`Números pares de 2 a 12: ${i}`)
+    }
+}
