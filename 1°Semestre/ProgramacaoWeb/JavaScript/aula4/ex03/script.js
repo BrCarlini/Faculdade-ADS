d860e9b50dcc40e881e696f595b22203e6548853
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-26 19:42:52 -0300

    ex03 da aula 4 - Em andamento

--- 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex03/script.js
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex03/script.js
@@ -4,5 +4,25 @@ usuário qual tabuada ele deseja visualizar. O usuário deverá responder digita
 número de 1 a 10 e a tabuada deverá ser exibida na página.
 */
 
+function clique(){
+    let caixaEntrada = document.getElementById('numero')
+    let dadoCaixa = Number(caixaEntrada.value)
+    let resp = document.getElementById('resp')
 
-let tabuada = parseInt(prompt("Informe a tabuada que deseja ver: "))
+    if(dadoCaixa <= 0 || dadoCaixa > 10){
+        alert('Informe número entre 1 e 10')
+    } else {
+        for(i = 1; i <= 10; i++){
+            let op = dadoCaixa * i
+
+            resp.innerText = `${dadoCaixa} x ${i} = ${op}` 
+
+            
+        }
+    }
+
+
+
+
+    console.log(typeof(dadoCaixa))
+}
