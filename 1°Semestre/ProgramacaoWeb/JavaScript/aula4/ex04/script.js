2ae22597fc8e13a1ca36303d702d686d576ec715
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-27 10:31:43 -0300

    Ex04 da aula4 concluído

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex04/script.js
@@ -0,0 +1,10 @@
+/*
+Faça um programa que exiba na página uma contagem regressiva de 10 até 0 usando laços 
+de repetição. Exemplo: 10... 9... 8... 7... 6... 5... 4... 3... 2... 1...
+
+*/
+
+for(i = 10; i >= 1; i--){
+    alert(`${i}...`)
+}
+alert('FIM!')
