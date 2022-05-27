a70d78e2f6dba749becd583359bbd6d8dd193bf2
Bruno Carlini <bruno-carlini@hotmail.com>
2022-05-27 10:22:10 -0300

    Ex03 da aula4 concluído

--- 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex03/script.js
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex03/script.js
@@ -7,22 +7,36 @@ número de 1 a 10 e a tabuada deverá ser exibida na página.
 function clique(){
     let caixaEntrada = document.getElementById('numero')
     let dadoCaixa = Number(caixaEntrada.value)
-    let resp = document.getElementById('resp')
+    let div = document.createElement('div')
+    let h4 = document.createElement('h4')
+
+    
 
     if(dadoCaixa <= 0 || dadoCaixa > 10){
         alert('Informe número entre 1 e 10')
     } else {
+        document.body.appendChild(div)
+        div.innerText = '======================================='
+
+        document.body.appendChild(h4)
+        h4.innerText = `Tabuada do ${dadoCaixa}`
+
         for(i = 1; i <= 10; i++){
             let op = dadoCaixa * i
-
-            resp.innerText = `${dadoCaixa} x ${i} = ${op}` 
-
+            let p = document.createElement('p')
             
+            document.body.appendChild(p)
+
+            p.innerText = `${dadoCaixa} x ${i} = ${op}`
+
+            //console.log(`${dadoCaixa} x ${i} = ${op}`)
         }
+
+        
     }
 
 
 
 
-    console.log(typeof(dadoCaixa))
+    
 }
