5f8f6d51d5fb882cde5cd16deb00fe5bf71ecdf4
Bruno Carlini <bruno-carlini@hotmail.com>
2022-06-13 19:01:00 -0300

    ...

---
+++ 1°Semestre/ProgramacaoWeb/JavaScript/aula4/ex07/script.js
@@ -0,0 +1,13 @@
+/*
+Crie um vetor de 10 posições, preencha com números aleatórios de 1 a 100 (pesquise como 
+gerar números aleatórios em JS) e exiba a média dos elementos em um input ao clicar num 
+botão.
+*/
+
+let lista = new Array(10)
+
+function gerarNum(){
+
+}
+
+
